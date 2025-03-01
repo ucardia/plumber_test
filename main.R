06f6da89b3369191f32073efa85b5dc1652f5ace
Curtis Petersen <clpurtis@gmail.com>
2025-02-28 16:17:04 -0800

    Test removing magick

--- main.R
+++ main.R
@@ -8,7 +8,19 @@
 #
 
 library(plumber)
-
+library(RMariaDB)
+library(DBI)
+library(tidyverse)
+library(glue)
+library(lubridate)
+library(httr)
+library(ggplot2)
+library(cowplot)
+library(gridExtra)
+library(elementalist)
+library(ggtext)
+# library(magick)
+library(ggwordcloud)
 
 #* @apiTitle Plumber Example API
 #* @apiDescription Plumber example description.
@@ -17,22 +29,6 @@ library(plumber)
 #* @param msg The message to echo
 #* @get /echo
 function(msg = "") {
-    
-    library(RMariaDB)
-    library(DBI)
-    library(tidyverse)
-    library(glue)
-    library(lubridate)
-    library(httr)
-    library(ggplot2)
-    library(cowplot)
-    library(gridExtra)
-    library(elementalist)
-    library(ggtext)
-    library(magick)
-    library(ggwordcloud)
-    
-    
     list(msg = paste0("The message is: '", msg, "'"))
 }
 
